a5d4742a8754661366e0217f659ccdb8a714e1f1
MarioBatalha <mariobatalha9@gmail.com>
2022-01-20 17:49:54 +0100

    add delete route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,5 +8,6 @@ const roomsController = new RoomsController();
 
 routes.get('/rooms', roomsController.index);
 routes.post('/rooms', roomsController.create);
+routes.delete('/rooms', roomsController.remove);
 
 export default routes;
