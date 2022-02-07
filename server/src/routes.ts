9d2f39b68514c4ea23ae96b6ba66b5e21c2c8201
MarioBatalha <mariobatalha9@gmail.com>
2022-02-07 18:32:39 +0100

    add routes create event

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 
 import RoomsController from './controllers/RoomsController';
+import EventsController from './controllers/EventsControllers';
 
 const routes = express.Router();
 
 const roomsController = new RoomsController();
+const eventsController =  new EventsController();
 
 routes.get('/rooms', roomsController.index);
 routes.post('/rooms', roomsController.create);
 routes.put('/rooms/:id_room', roomsController.update);
 routes.delete('/rooms', roomsController.remove);
+
+routes.post('/events', eventsController.create);
 
 export default routes;  
