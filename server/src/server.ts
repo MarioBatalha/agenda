2ed0f25b1bc3042bfa7be7ac8225f29c9e31f966
MarioBatalha <mariobatalha9@gmail.com>
2022-02-10 16:57:32 +0100

    add errors from celebrate

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+app.use(errors());
 
 app.use(routes);
 app.listen(3333);
